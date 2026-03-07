95a8fd9af9912ba859ff074f0529037235387f8e
fullstackmachina <misscyborgtech@gmail.com>
2026-03-07 14:28:17 -0500

    refactor for better readability and add comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 // ELEMENTS
-const search = document.querySelector(".search-container");
+const searchContainer = document.querySelector(".search-container");
 const employeesGallery = document.getElementById("gallery");
 
 // GLOBAL VARIABLES
 let employeesArray = [];
-let employeesIndex = 0;
+let currentEmployeeIndex = 0;
 
 // INIZIALIZATION
 getEmployees();
 
 // FETCH FUNCTIONS
+// Fetch 12 employees from the Random User API
 async function getEmployees() {
   try {
     const results = await fetch("https://randomuser.me/api/?results=12");
@@ -18,60 +19,75 @@ async function getEmployees() {
     displayEmployees();
   } catch (error) {
     console.error(error);
-  } /*finally {
-  }*/
+  }
 }
 
 // UI HELPER FUNCTIONS
+// Render all employee cards in the gallery
 function displayEmployees() {
+  let employeesHTML = "";
   employeesArray.forEach((employee, index) => {
-    const employeeInfo = `
+    const { name, picture, location, email } = employee;
+    const fullName = `${name.first} ${name.last}`;
+    const cityState = `${location.city}, ${location.state}`;
+
+    employeesHTML += `
          <div class="card" data-index=${index}>
                     <div class="card-img-container">
-                        <img class="card-img" src="${employee.picture.medium}" alt="employee's picture">
+                        <img class="card-img" src="${picture.medium}" alt="employee's picture">
                     </div>
                     <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${employee.name.first + " " + employee.name.last}</h3>
-                        <p class="card-text">${employee.email}</p>
-                        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+                        <h3 class="card-name cap">${fullName}</h3>
+                        <p class="card-text">${email}</p>
+                        <p class="card-text cap">${cityState}</p>
                     </div>
                 </div>
         `;
-    employeesGallery.insertAdjacentHTML("beforeend", employeeInfo);
   });
+  employeesGallery.insertAdjacentHTML("beforeend", employeesHTML);
 }
 
 // EVENT LISTENERS
+// Open employee modal when a card is clicked
 employeesGallery.addEventListener("click", (e) => {
   const galleryCard = e.target.closest(".card");
   if (!galleryCard) {
     return;
   } else {
-    employeesIndex = parseInt(galleryCard.dataset.index);
-    showEmployeeModal(employeesIndex);
+    currentEmployeeIndex = parseInt(galleryCard.dataset.index);
+    showEmployeeModal(currentEmployeeIndex);
   }
 });
 
+// Create and display the employee modal
 function showEmployeeModal(index) {
-  const modalContainer = document.querySelector(".modal-container");
-
-  if (modalContainer) {
-    modalContainer.remove();
-  }
+  // Remove existing modal if one is already open
+  document.querySelector(".modal-container")?.remove();
   const employee = employeesArray[index];
+  const { name, location, picture, email, cell, dob } = employee;
+  const birthday = new Date(dob.date);
+  const month = birthday.getMonth() + 1;
+  const day = birthday.getDate();
+  const year = birthday.getFullYear();
+  const formattedBirthday = `${month}/${day}/${year}`;
+  const fullName = name.first + " " + name.last;
+  const fullAddress = ` ${location.street.number} ${location.street.name}, ${location.city}, ${location.state} ${location.postcode}
+`;
+  const modalLocation = location.city;
+
   const employeeModal = `
    <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="${employee.picture.large}" alt="employee's picture">
-                        <h3 id="name" class="modal-name cap">${employee.name.first + " " + employee.name.last}</h3>
-                        <p class="modal-text">${employee.email}</p>
-                        <p class="modal-text cap">${employee.location.city}</p>
+                        <img class="modal-img" src="${picture.large}" alt="employee's picture">
+                        <h3  class="modal-name cap">${fullName}</h3>
+                        <p class="modal-text">${email}</p>
+                        <p class="modal-text cap">${modalLocation}</p>
                         <hr>
-                        <p class="modal-text">${employee.cell}</p>
-                        <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode} </p>
-                        <p class="modal-text">Birthday: ${employee.dob.date}</p>
+                        <p class="modal-text">${cell}</p>
+                        <p class="modal-text">${fullAddress} </p>
+                        <p class="modal-text">Birthday: ${formattedBirthday}</p>
                     </div>
                 </div>
 
@@ -85,6 +101,7 @@ function showEmployeeModal(index) {
   document.body.insertAdjacentHTML("beforeend", employeeModal);
   const newModalContainer = document.querySelector(".modal-container");
   const closeButton = document.getElementById("modal-close-btn");
+  // Close the modal when the X button is clicked
   closeButton.addEventListener("click", () => {
     newModalContainer.remove();
   });
