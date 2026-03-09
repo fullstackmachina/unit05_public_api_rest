7a380c3674bf9d4727cbae4f6ff96e3542b077e0
fullstackmachina <misscyborgtech@gmail.com>
2026-03-09 13:10:02 -0400

    add search functionality

--- js/script.js
+++ js/script.js
@@ -8,15 +8,18 @@ let currentEmployeeIndex = 0;
 
 // INIZIALIZATION
 getEmployees();
+displaySearch();
 
 // FETCH FUNCTIONS
 // Fetch 12 employees from the Random User API
 async function getEmployees() {
   try {
-    const results = await fetch("https://randomuser.me/api/?results=12");
+    const results = await fetch(
+      "https://randomuser.me/api/?results=12&nat=us,ca,gb,au",
+    );
     const data = await results.json();
     employeesArray = data.results;
-    displayEmployees();
+    displayEmployees(employeesArray);
   } catch (error) {
     console.error(error);
   }
@@ -24,9 +27,10 @@ async function getEmployees() {
 
 // UI HELPER FUNCTIONS
 // Render all employee cards in the gallery
-function displayEmployees() {
+function displayEmployees(arr) {
+  employeesGallery.innerHTML = " ";
   let employeesHTML = "";
-  employeesArray.forEach((employee, index) => {
+  arr.forEach((employee, index) => {
     const { name, picture, location, email } = employee;
     const fullName = `${name.first} ${name.last}`;
     const cityState = `${location.city}, ${location.state}`;
@@ -65,12 +69,13 @@ function showEmployeeModal(index) {
   document.querySelector(".modal-container")?.remove();
   const employee = employeesArray[index];
   const { name, location, picture, email, cell, dob } = employee;
+  const fullName = name.first + " " + name.last;
   const birthday = new Date(dob.date);
   const month = birthday.getMonth() + 1;
   const day = birthday.getDate();
   const year = birthday.getFullYear();
   const formattedBirthday = `${month}/${day}/${year}`;
-  const fullName = name.first + " " + name.last;
+
   const fullAddress = ` ${location.street.number} ${location.street.name}, ${location.city}, ${location.state} ${location.postcode}
 `;
   const modalLocation = location.city;
@@ -106,3 +111,42 @@ function showEmployeeModal(index) {
     newModalContainer.remove();
   });
 }
+
+// EXTRA CREDITS
+function displaySearch() {
+  const formSearch = `
+     <form>
+                            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                        </form>
+    `;
+
+  searchContainer.insertAdjacentHTML("beforeend", formSearch);
+}
+
+const userInput = document.getElementById("search-input");
+
+userInput.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  const searchValue = e.target.value.toLowerCase().trim();
+  const displaySearchResults = employeesArray.filter((employee) => {
+    const { name } = employee;
+    const fullName = (name.first + " " + name.last).toLowerCase();
+    if (fullName.includes(searchValue)) {
+      return employee;
+    }
+  });
+  if (displaySearchResults.length === 0) {
+    employeesGallery.innerHTML = "";
+    const emptyStateMessage = document.createElement("h3");
+    emptyStateMessage.textContent = "No employees found.";
+    employeesGallery.appendChild(emptyStateMessage);
+  } else {
+    displayEmployees(displaySearchResults);
+  }
+});
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
